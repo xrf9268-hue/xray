90391974fa81abab138b0c4bdc1e5fdf82cf541e
xrf9268-hue <xrf9268@gmail.com>
2026-01-25 02:49:26 -0800

    commands: clarify log output handling

--- commands/status.sh
+++ commands/status.sh
@@ -6,7 +6,14 @@ HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 main() {
   core::init "${@}"
   local ver="unknown"
-  [[ -x "$(xray::bin)" ]] && ver="$("$(xray::bin)" -version 2> /dev/null | awk 'NR==1{print $2}')"
-  core::log info "Xray" "$(printf '{"version":"%s","active_confdir":"%s"}' "${ver}" "$(xray::active)")"
+  local xray_bin
+  xray_bin="$(xray::bin)"
+  if [[ -x "${xray_bin}" ]]; then
+    ver="$("${xray_bin}" -version 2> /dev/null | awk 'NR==1{print $2}')"
+  fi
+
+  local active_confdir
+  active_confdir="$(xray::active)"
+  core::log info "Xray" "$(printf '{"version":"%s","active_confdir":"%s"}' "${ver}" "${active_confdir}")"
 }
 main "${@}"
